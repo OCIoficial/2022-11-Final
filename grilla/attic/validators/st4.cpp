ac841e402c3b18cb7bb7e8183cab64083126bec2
Daniel Báez <mastil.daniel@gmail.com>
2022-11-14 03:54:01 -0300

    validadores grilla

---
+++ grilla/attic/validators/st4.cpp
@@ -0,0 +1,21 @@
+#include "testlib.h"
+
+using namespace std;
+
+int main(){
+	registerValidation();
+	int n = inf.readInt(1, 1e3, "n");
+	inf.readSpace();
+	int m = inf.readInt( max(1,2/n) , min(1e3,1e5/n) , "m");
+    inf.readSpace();
+	int k = inf.readInt(2, n*m, "k");
+	inf.readEoln();
+    for (int i=0;i<n;i++){
+        for (int j=0;j<m;j++){
+            if (j) inf.readSpace();
+            inf.readInt(1,k,"color [" + to_string(i) + " , " + to_string(j) + "]" );
+        }
+        inf.readEoln();
+    }
+	inf.readEof();
+}
